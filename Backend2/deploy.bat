f3e11f7a705b313a4f8124bb454ac8485e695a7d
Gajendra Pratap Singh <gajendra.2024023054@tulas.edu.in>
2026-06-11 05:39:23 +0530

    fixed deploy script

--- Backend2/deploy.bat
+++ Backend2/deploy.bat
@@ -2,6 +2,6 @@
 echo Deploying...
 
 :: Run remote commands over SSH using your alias
-ssh api_naisha "cd ~/public_html && git pull origin main && cd Backend2 && npm ci --omit=dev && touch ~/public_html/tmp/restart.txt"
+ssh api_naisha "cd ~/public_html && git pull origin main && cd Backend2 && /var/www/7a240458-fb47-42de-be89-6155cb6966f6/.nvm/versions/node/v26.3.0/bin/npm ci --omit=dev && touch ~/public_html/tmp/restart.txt"
 
 echo Deployed successfully
